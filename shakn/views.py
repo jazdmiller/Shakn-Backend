82f0f12feffad89db932e723052853f3ce03ecbc
Jazmine Miller <jazminedmiller@gmail.com>
2022-07-14 11:35:01 -0400

    added view for index product list

--- shakn/views.py
+++ shakn/views.py
@@ -1,3 +1,9 @@
+from itertools import product
 from django.shortcuts import render
+from .models import Product, Review
 
 # Create your views here.
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'shakn/product_list.html', {'products': products})
